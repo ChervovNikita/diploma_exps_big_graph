05551974751144a4b32f8d6bacfc0d646d81bccb
Nikita Chervov <gcnikitachervov@gmail.com>
2026-01-25 09:22:32 +0000

    t

--- validate_pipeline/bug_with_semi/run_validation_graph_based_v2.sh
+++ validate_pipeline/bug_with_semi/run_validation_graph_based_v2.sh
@@ -12,7 +12,7 @@ for SEED in 42; do
     mkdir -p "logs"
     mkdir -p "checkpoints"
 
-    # python generate_split_v2.py >> "logs/run_validation_graph_based_one_node_v2_seed_${SEED}.log"
+    python generate_split_v2.py >> "logs/run_validation_graph_based_one_node_v2_seed_${SEED}.log"
     python -m bug_with_semi.train_simple_mask_model_graph_based >> "logs/run_validation_graph_based_one_node_v2_seed_${SEED}.log"
     python -m bug_with_semi.run_on_test_graph_based >> "logs/run_validation_graph_based_one_node_v2_seed_${SEED}.log"
     python -m score >> "logs/run_validation_graph_based_one_node_v2_seed_${SEED}.log"
